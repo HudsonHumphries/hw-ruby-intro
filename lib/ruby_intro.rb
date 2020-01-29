7a04c51460a249e53ebbf4ad445d7715f81a9465
EC2 Default User <ec2-user@ip-172-31-43-252.us-east-2.compute.internal>
2020-01-29 20:11:10 +0000

    Completed part 3, finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -71,4 +71,32 @@ end
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn,price)
+    if(isbn.length == 0 || price <= 0)
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  
+  def price=(price)
+    @price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def price_as_string
+    return "$"+('%.2f' % price).to_s
+  end
 end
+
